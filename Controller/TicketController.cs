58778d3200a792439c4343a0a038824cabd394da
Luis <lgimenez@frba.utn.edu.ar>
2022-05-09 22:07:52 -0300

    se corrige namespace y se agrega sln

---
+++ Controller/TicketController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using RepairManager.Entity;
+
+namespace RepairManager.Controller;
+
+
+[ApiController]
+[Route("api/v1/ticket")]
+public class TicketController : ControllerBase
+{
+    [HttpGet]
+    public ActionResult<List<Ticket>> getTicketList(){
+        List<Ticket> ticketList = new List<Ticket>();
+     
+        ticketList.Add(new Ticket(){
+            Id=1,
+            CustomerName="clienteTest",
+            CustomerPhone="phoneTest",
+            CustomerAddress="AddressTest",
+            TaskDescription="DescriptionTest"});
+    
+        return ticketList;
+    }
+
+}
---
+++ Entity/Ticket.cs
@@ -0,0 +1,11 @@
+namespace RepairManager.Entity;
+
+public class Ticket{
+    public long? Id { get; set; }
+    public string? CustomerName { get; set; }
+    public string? CustomerAddress { get; set; }
+    public string? CustomerPhone { get; set; }
+    public string? TaskDescription { get; set; }
+}
+
+
